f95ef89553ae1630e05354471feaddfdb426faf2
Konrad Wrzos <konrad@travis-ci.org>
2024-07-23 18:04:49 +0200

    OSX fixes for ruby 3.X

--- build.sh
+++ build.sh
@@ -163,6 +163,8 @@ ensure_gpg_key
 rm -f ~/.rvmrc
 announce rvm reload
 announce rvm use ruby-2.4.2
+announce curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+announce curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 announce curl -sSL https://get.rvm.io | bash -s stable
 announce rvm cleanup all
 fold_end rvm.1
