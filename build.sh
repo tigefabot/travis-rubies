6473034c0e4b6afa01c61595921f8dc79cab994f
Konrad Wrzos <konrad@travis-ci.org>
2024-07-24 14:19:03 +0200

    OSX fixes for ruby 3.X

--- build.sh
+++ build.sh
@@ -268,7 +268,9 @@ ruby-2.*)
 # Ruby YJIT
 ruby-3.*)
   rust_setup
-  announce rvm install --with-openssl-dir="/usr/local/opt/openssl@1.0" $RUBY $EXTRA_FLAGS --enable-yjit --verify-downloads 1 $MOVABLE_FLAG --disable-install-doc -C --without-tcl,--without-tk,--without-gmp
+  announce brew install openssl@1.1
+  OPENSSL_FLAGS="-C --with-openssl-dir=/usr/local/opt/openssl@1.1"
+  announce rvm install --with-openssl-dir="/usr/local/opt/openssl@1.1" $RUBY $EXTRA_FLAGS --enable-yjit --verify-downloads 1 $MOVABLE_FLAG --disable-install-doc -C --without-tcl,--without-tk,--without-gmp
   ;;
 jruby-head)
   update_mvn 3.3.9
