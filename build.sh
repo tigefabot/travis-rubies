d9c00d2f18b21aeb7a946c5a0ee56d7f5832f1ef
Konrad Wrzos <konrad@travis-ci.org>
2024-07-24 12:16:40 +0200

    OSX fixes for ruby 3.X

--- build.sh
+++ build.sh
@@ -165,7 +165,7 @@ announce rvm reload
 announce curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 announce curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 announce curl -sSL https://get.rvm.io | bash -s stable
-announce rvm use ruby-3.2.0
+announce rvm use --install 2.4.2
 announce rvm cleanup all
 fold_end rvm.1
 
@@ -195,7 +195,7 @@ if command -v sw_vers >> /dev/null; then
   fold_start rvm.4 "OSX specific setup"
   announce rvm autolibs homebrew
   announce rvm get stable --auto-dotfiles
-  announce rvm use --install 3.2.0
+  announce rvm use --install 2.4.2
   announce sudo mkdir -p /etc/openssl
   announce sudo chown -R $USER: /etc/openssl
   # announce rvm use 2.0.0 --fuzzy
